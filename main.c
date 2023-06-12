e2f1673e0a6c64feb752ec7e0f5bf34e11d85211
lsileoni <lsileoni@proton.me>
2023-06-12 14:24:54 +0300

    Testing norminette

--- main.c
+++ main.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsileoni <lsileoni@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/12 14:24:01 by lsileoni          #+#    #+#             */
+/*   Updated: 2023/06/12 14:24:25 by lsileoni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <pthread.h>
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
+int	main(void)
 {
 	printf("Fixed!\n");
 	return (0);
