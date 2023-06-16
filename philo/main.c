eeab9ff7c740346d91637cff9b4f2a9f7c04189e
lsileoni <lsileoni@proton.me>
2023-06-16 19:30:48 +0300

    Added philosopher initialization logic - doesn't cover case for 1 or 2 philosophers.

--- philo/main.c
+++ philo/main.c
@@ -6,24 +6,29 @@
 /*   By: lsileoni <lsileoni@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 14:24:01 by lsileoni          #+#    #+#             */
-/*   Updated: 2023/06/16 16:58:54 by lsileoni         ###   ########.fr       */
+/*   Updated: 2023/06/16 19:29:06 by lsileoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	init_philos(t_philo **arr, t_args *args)
-{
-	*arr = malloc(sizeof(t_philo *) * args->philo_count);
-	return (1);
-}
-
 int	main(int argc, char **argv)
 {
 	const t_args	args = parse_args(argc, argv);
 	t_philo			*philos;
+	size_t			i;
 
 	if (!args.set)
 		return (ft_printf("Issue in parsing arguments!\n"));
+	if (!init_philos(&philos, &args))
+		return (ft_printf("Issue in initializing philosophers!\n"));
+	i = 0;
+	while (i < args.philo_count)
+	{
+		ft_printf("philos[%d]->state: %d\n", i, philos[i].state);
+		ft_printf("philos[%d]->left_fork: %p\n", i, philos[i].left_fork);
+		ft_printf("philos[%d]->right_fork: %p\n", i, philos[i].right_fork);
+		i++;
+	}
 	return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: lsileoni <lsileoni@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 14:14:28 by lsileoni          #+#    #+#             */
-/*   Updated: 2023/06/16 17:13:39 by lsileoni         ###   ########.fr       */
+/*   Updated: 2023/06/16 19:28:56 by lsileoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ typedef struct s_args
 
 typedef struct s_philo
 {
-	t_args			*params;
+	t_args			params;
 	pthread_mutex_t *left_fork;
 	pthread_mutex_t *right_fork;
 	unsigned int	ms_state;
@@ -51,5 +51,6 @@ typedef struct s_philo
 }	t_philo;
 
 t_args	parse_args(int argc, char **argv);
+int	init_philos(t_philo **philos, const t_args *args);
 
 #endif
