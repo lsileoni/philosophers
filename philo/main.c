c9bc892a5dab220a7d69a7028f77dcd26b168f06
lsileoni <lsileoni@proton.me>
2023-06-16 17:15:25 +0300

    Returning out of the function instead of continuing

--- philo/main.c
+++ philo/main.c
@@ -6,17 +6,24 @@
 /*   By: lsileoni <lsileoni@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 14:24:01 by lsileoni          #+#    #+#             */
-/*   Updated: 2023/06/16 14:18:45 by lsileoni         ###   ########.fr       */
+/*   Updated: 2023/06/16 16:58:54 by lsileoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+int	init_philos(t_philo **arr, t_args *args)
+{
+	*arr = malloc(sizeof(t_philo *) * args->philo_count);
+	return (1);
+}
+
 int	main(int argc, char **argv)
 {
 	const t_args	args = parse_args(argc, argv);
+	t_philo			*philos;
 
 	if (!args.set)
-		ft_printf("Issue in parsing arguments!\n");
+		return (ft_printf("Issue in parsing arguments!\n"));
 	return (0);
 }
